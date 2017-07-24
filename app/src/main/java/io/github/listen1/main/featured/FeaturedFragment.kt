15f2a26ee6baad709f8c034c400991da9de1cf00
何祥源 <hexiangyuan@ezbuy.com>
2017-07-24 21:09:50 +0800

    Jsoup

--- app/src/main/java/io/github/listen1/main/featured/FeaturedFragment.kt
+++ app/src/main/java/io/github/listen1/main/featured/FeaturedFragment.kt
@@ -1,26 +1,15 @@
 package io.github.listen1.main.featured
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import io.github.listen1.R
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [FeaturedFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [FeaturedFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FeaturedFragment : Fragment() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -29,6 +18,18 @@ class FeaturedFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater!!.inflate(R.layout.fragment_featured, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        Thread({
+            try {
+                val document:Document = Jsoup.connect("http://www.xiami.com/collect/recommend/page/1").get()
+                val elements = document.select("block_list clearfix")
+            }catch (e:Exception){
+
+            }
+        }).start()
     }
 
     companion object {
---
+++ app/src/main/java/io/github/listen1/main/featured/Net163Fragment.kt
@@ -0,0 +1,17 @@
+package io.github.listen1.main.featured
+
+import android.support.v4.app.Fragment
+
+/**
+ * Created by hexiangyuan on 2017/7/24.
+ * feature:
+ */
+class Net163Fragment : Fragment() {
+
+
+    companion object {
+        fun newInstance(): Net163Fragment {
+            return newInstance()
+        }
+    }
+}
